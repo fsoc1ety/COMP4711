6f546bd449ab6961f959bfb9c64e604d7d4e9952
Derrick <drick89@gmail.com>
2016-01-14 19:50:46 -0800

    testing github in netbeans

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "Test github"
+        echo "Test github push from netbeans"
         ?>
     </body>
 </html>
