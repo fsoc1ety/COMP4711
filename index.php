3bef2581ef0df0f68cec7168a13949c0e40b593a
Derrick <drick89@gmail.com>
2016-01-14 20:05:09 -0800

    Try it: 3

--- index.php
+++ index.php
@@ -16,6 +16,11 @@ and open the template in the editor.
             $level =10;
             echo 'Hi, my name '.$name,'. and I am a level '.$level.'
             '.$what;
+            echo '<br/>';
+            $hoursworked = 10;
+            $rate = 12;
+            $total = $hoursworked * $rate;
+            echo 'You owe me '.$total;
         ?>
     </body>
 </html>
