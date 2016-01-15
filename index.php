fbfa63575c225e3d9b8ac0579113dd16d573eb0b
Derrick <drick89@gmail.com>
2016-01-14 20:45:40 -0800

    Try it: 7

--- index.php
+++ index.php
@@ -24,38 +24,17 @@ and open the template in the editor.
 
         function winner($token,$position)
             {
-                $won = false;
-                if (($position[0] == $token) &&
-                    ($position[1] == $token)&&
-                    ($position[2] == $token))
+                for($row=0; $row<3; $row++)
+                {
+                    $result = true;
+                    for($col=0; $col<3; $col++)
                     {
-                        $won = true;
+                        if ($position[3*$row+$col] != $token)
+                        {
+                            $result = false;
+                        }
                     }
-                else if (($position[3] == $token) &&
-                         ($position[4] == $token) &&
-                         ($position[5] == $token))
-                    {
-                        $won = true;
-                    }
-                else if (($position[6] == $token) &&
-                         ($position[7] == $token) &&
-                         ($position[8] == $token))
-                   {
-                       $won = true;
-                   }
-                else if (($position[0] == $token) &&
-                         ($position[4] == $token) &&
-                         ($position[8] == $token))
-                   {
-                       $won = true;
-                   }
-                else if (($position[2] == $token) &&
-                         ($position[4] == $token) &&
-                         ($position[6] == $token))
-                   {
-                       $won = true;
-                   }
-                    return $won;
+                }
             }
         ?>
     </body>
