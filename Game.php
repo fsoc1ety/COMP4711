d448e92debac1484966247cf9fdd96344aa0538d
Derrick <drick89@gmail.com>
2016-01-15 02:26:48 -0800

    Revert "Try it: 8"
    This reverts commit 9bce0f6950653df4f1fd3dbd6d4b56329abf7e5f.

--- Game.php
+++ Game.php
@@ -14,20 +14,11 @@
 class Game {
     var $position;
     
-    // constructor 
     function __construct($squares) 
     {
-        $game = "---------";
-        
-        // checks to see if game is currently active
-        if (!empty($squares))
-        {
-            $game = $squares;
-        }
-        $this->position = str_split($game);
+        $this->position = str_split($squares);
     }
     
-    // win algorithm
     function winner($token)
     {
         for ($col = 0; $col < 3; $col++) 
@@ -40,7 +31,7 @@ class Game {
             }
         }
         
-        for ($row = 0; $row < 9; $row += 3) 
+        for ($row = 0; $row < 9; $row+= 3) 
         {
             if ($this->position[$row] == $token && 
                 $this->position[$row + 1] == $token && 
@@ -50,41 +41,4 @@ class Game {
             }
         }
     }
-    
-    // displays board
-    function display()
-    {
-        echo '<table colspan="3" style="font-size:large; font-weight:bold" width="400" height="200">';
-        echo '<tr>';
-        for ($pos = 0; $pos < 9; $pos++)
-        {
-            echo $this->show_cell($pos);
-            if ($pos %3 == 2)
-            {
-                echo '</tr><tr>';
-            }
-        }
-        echo '</tr>';
-        echo '</table>';
-    }
-    
-    // determines which game piece to show
-    function show_cell($which)
-    {
-        $token = $this->position[$which];
-        if ($token <> '-')
-        {
-            return '<td>' . $token . '</td>';
-        }
-            
-        $this->newposition = $this->position;
-        $this->newposition[$which] = 'o';
-        $move = implode($this->newposition);
-        $link = '?board=' . $move;
-        return '<td><a href="' . $link . '">-</a></td>';
-    }
-    
-   
 }
-
-
